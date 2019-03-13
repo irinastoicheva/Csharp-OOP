b1b97c99906e20c984181bef97d836523c784b07
irinastoicheva <irinastoicheva@mail.bg>
2019-03-13 23:34:53 +0200

    commit

---
+++ 02. Working with Abstraction - Lab/P03.StudentSystemCatalog/CommandParser.cs
@@ -0,0 +1,18 @@
+﻿namespace P03.StudentSystemCatalog
+{
+    using System;
+    using System.Linq;
+
+    public class CommandParser
+    {
+        public Command Parse(string input)
+        {
+            string[] parts = input.Split();
+            string name = parts[0];
+            string[] arguments = parts.Skip(1).ToArray();
+            Command command = new Command(name, arguments);
+
+            return command;
+        }
+    }
+}
---
+++ 02. Working with Abstraction - Lab/P03.StudentSystemCatalog/Startup.cs
@@ -0,0 +1,43 @@
+﻿namespace P03.StudentSystemCatalog
+{
+    using System;
+    class StartUp
+    {
+        static void Main()
+        {
+            StudentSystem studentSystem = new StudentSystem();
+
+            while (true)
+            {
+                CommandParser commandParser = new CommandParser();
+                Command command = commandParser.Parse(Console.ReadLine());
+
+                try
+                {
+                    if (command.Name == "Create")
+                    {
+                        string name = command.Arguments[0];
+                        int age = int.Parse(command.Arguments[1]);
+                        double grade = double.Parse(command.Arguments[2]);
+                        studentSystem.Add(name, age, grade);
+                    }
+                    else if (command.Name == "Show")
+                    {
+                        string name = command.Arguments[0];
+                        Student student = studentSystem.Show(name);
+                        Console.WriteLine(student);
+                    }
+                    else if (command.Name == "Exit")
+                    {
+                        break;
+                    }
+                }
+                catch
+                {
+                    continue;
+                }
+                
+            }
+        }
+    }
+}
