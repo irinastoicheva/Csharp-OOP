d5cd8bb487af31f50bc124f51ffca2387a0e1ab3
irinastoicheva <irinastoicheva@mail.bg>
2019-03-19 16:49:52 +0200

    commit

---
+++ 02. Working with Abstraction - Exersice/P06_Sneaking/Enemy.cs
@@ -0,0 +1,14 @@
+﻿namespace P06_Sneaking
+{
+    public class Enemy
+    {
+        public Enemy(char name, Position position)
+        {
+            this.Name = name;
+            this.Position = position;
+        }
+        public char Name { get; set; }
+
+        public Position Position { get; set; }
+    }
+}
---
+++ 02. Working with Abstraction - Exersice/P06_Sneaking/Position.cs
@@ -0,0 +1,14 @@
+﻿namespace P06_Sneaking
+{
+    public class Position
+    {
+        public Position(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+        public int X { get; set; }
+
+        public int Y { get; set; }
+    }
+}
