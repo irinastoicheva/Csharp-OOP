6c991f3bfd1af3a18cdf8ad45651521254f29e8e
irinastoicheva <irinastoicheva@mail.bg>
2019-03-20 14:22:58 +0200

    commit

---
+++ 03. Encapsulation - Exercise/P02.ClassBoxDataValidation/Box.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace P02.ClassBoxDataValidation
+{
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double length, double width, double height)
+        {
+            this.Length = length;
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public double Length
+        {
+            get => this.length;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException($"Length cannot be zero or negative. ");
+                }
+
+                this.length = value;
+            }
+        }
+        public double Width
+        {
+            get => this.width;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException($"Width cannot be zero or negative. ");
+                }
+
+                this.width = value;
+            }
+        }
+        public double Height
+        {
+            get => this.height;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException($"Height cannot be zero or negative. ");
+                }
+
+                this.height = value;
+            }
+        }
+
+        public double CalculateSurfaceArea()
+        {
+            return this.length * this.height * 2 + this.width * this.height * 2 + this.length * this.width * 2;
+        }
+
+        public double CalculateLateralSurfaceArea()
+        {
+            return this.length * this.height * 2 + this.width * this.height * 2;
+        }
+
+        public double CalculateVolume()
+        {
+            return this.length * this.width * this.height;
+        }
+    }
+}
---
+++ 03. Encapsulation - Exercise/P02.ClassBoxDataValidation/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace P02.ClassBoxDataValidation
+{
+    using System;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+            try
+            {
+                Box box = new Box(length, width, height);
+                Console.WriteLine($"Surface Area - {box.CalculateSurfaceArea():F2}");
+                Console.WriteLine($"Lateral Surface Area - {box.CalculateLateralSurfaceArea():F2}");
+                Console.WriteLine($"Volume - {box.CalculateVolume():F2}");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
