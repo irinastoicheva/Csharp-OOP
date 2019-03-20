0706a4361cd5966183909afbf5a0ecbf10de9ec6
irinastoicheva <irinastoicheva@mail.bg>
2019-03-20 14:07:16 +0200

    commit

---
+++ 03. Encapsulation - Exercise/P01.ClassBox/Box.cs
@@ -0,0 +1,31 @@
+﻿namespace P01.ClassBox
+{
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double length, double width, double height)
+        {
+            this.length = length;
+            this.width = width;
+            this.height = height;
+        }
+
+        public double CalculateSurfaceArea()
+        {
+            return this.length * this.height * 2 + this.width * this.height * 2 + this.length * this.width * 2;
+        }
+
+        public double CalculateLateralSurfaceArea()
+        {
+            return this.length * this.height * 2 + this.width * this.height * 2;
+        }
+
+        public double CalculateVolume()
+        {
+            return this.length * this.width * this.height;
+        }
+    }
+}
