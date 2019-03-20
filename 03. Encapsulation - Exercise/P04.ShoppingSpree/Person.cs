b55c0368bbbddbef340a3da5adab07904263db01
irinastoicheva <irinastoicheva@mail.bg>
2019-03-20 16:54:54 +0200

    commit

---
+++ 03. Encapsulation - Exercise/P04.ShoppingSpree/Person.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P04.ShoppingSpree
+{
+    public class Person
+    {
+        private string name;
+        private decimal money;
+        private List<Product> bag;
+
+        public Person(string name, decimal money)
+        {
+            this.Name = name;
+            this.Money = money;
+            this.bag = new List<Product>();
+        }
+        public string Name
+        {
+            get => this.name;
+            private set
+            {
+                if (value.Length == 0 || value == " ")
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+
+                this.name = value;
+            }
+        }
+        public decimal Money
+        {
+            get => this.money;
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                }
+
+                this.money = value;
+            }
+        }
+
+        public void AddProduct(Product product)
+        {
+            this.bag.Add(product);
+            this.money -= product.Cost;
+        }
+
+        public override string ToString()
+        {
+            if (this.bag.Count > 0)
+            {
+                return $"{this.Name} - {string.Join(", ", this.bag)}";
+            }
+            else
+            {
+                return $"{this.Name} - Nothing bought ";
+            }
+        }
+    }
+}
---
+++ 03. Encapsulation - Exercise/P04.ShoppingSpree/Startup.cs
@@ -0,0 +1,79 @@
+﻿namespace P04.ShoppingSpree
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            List<Person> people = new List<Person>();
+            List<Product> products = new List<Product>();
+
+            string[] peopleInformation = Console.ReadLine().Split(";");
+            for (int i = 0; i < peopleInformation.Length; i++)
+            {
+                string[] personInformation = peopleInformation[i].Split("=");
+                string name = personInformation[0];
+                decimal money = decimal.Parse(personInformation[1]);
+
+                try
+                {
+                    Person person = new Person(name, money);
+                    people.Add(person);
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    return;
+                }
+            }
+
+            string[] productsInformation = Console.ReadLine().Split(";", StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < productsInformation.Length; i++)
+            {
+                string[] productInformation = productsInformation[i].Split("=", StringSplitOptions.RemoveEmptyEntries);
+                string name = productInformation[0];
+                decimal cost = decimal.Parse(productInformation[1]);
+                try
+                {
+                    products.Add(new Product(name, cost));
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine(ex.Message);
+                    return;
+                }
+            }
+
+            string command = Console.ReadLine(); ;
+            while (command != "END")
+            {
+                string[] parts = command.Split();
+                Person person = people.FirstOrDefault(x => x.Name == parts[0]);
+                Product product = products.FirstOrDefault(x => x.Name == parts[1]);
+                if (person != null && product != null)
+                {
+                    if (person.Money >= product.Cost)
+                    {
+                        person.AddProduct(product);
+                        Console.WriteLine($"{person.Name} bought {product.Name}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{person.Name} can't afford {product.Name}");
+                    }
+                }
+
+                command = Console.ReadLine();
+            }
+
+            foreach (var item in people)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
