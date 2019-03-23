d193ae087b9a54e2a75ebe65f6ecf3db7f18d89b
irinastoicheva <irinastoicheva@mail.bg>
2019-03-23 22:53:13 +0200

    commit

---
+++ 03. Inheritance - Exercise/P01.Person/Startup.cs
@@ -0,0 +1,22 @@
+﻿namespace P01.Person
+{
+    using System;
+    public class Startup
+    {
+        public static void Main()
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            try
+            {
+                Child child = new Child(name, age);
+                Console.WriteLine(child);
+            }
+            catch (ArgumentException ae)
+            {
+                Console.WriteLine(ae.Message);
+            }
+        }
+    }
+}
