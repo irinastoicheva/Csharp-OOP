fba94e697571a01b9aaa021c694606a36431cd1e
irinastoicheva <irinastoicheva@mail.bg>
2019-03-27 11:23:19 +0200

    commit

---
+++ 05. Polymorphism - Lab/P02.Animals/Animal.cs
@@ -0,0 +1,18 @@
+﻿namespace Animals
+{
+    public class Animal
+    {
+        private string name;
+        private string favoriteFood;
+        public Animal(string name, string food)
+        {
+            this.name = name;
+            this.favoriteFood = food;
+        }
+
+        public virtual string ExplainSelf()
+        {
+            return $"I am {this.name} and my fovourite food is {this.favoriteFood}";
+        }
+    }
+}
