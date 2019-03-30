a40547da61b39ef23c77895b21c2ebd3a5dad22e
irinastoicheva <irinastoicheva@mail.bg>
2019-03-30 20:55:31 +0200

    commit

---
+++ 07. Reflection and Attributes - Lab/P02.HighQualityMistakes/Startup.cs
@@ -0,0 +1,10 @@
+﻿using System;
+public class Startup
+{
+    public static void Main()
+    {
+        Spy spy = new Spy();
+        string result = spy.AnalyzeAcessModifiers("Hacker");
+        Console.WriteLine(result);
+    }
+}
