9686cdde7b84cc2296eb0b10ffe27ad84c9a0dfb
irinastoicheva <irinastoicheva@mail.bg>
2019-03-31 19:28:39 +0300

    commit

---
+++ 07. Reflection and Attributes - Lab/P06.CodeTracker/Tracker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+public class Tracker
+{
+    public void PrintMethodsByAuthor()
+    {
+        var type = typeof(StartUp);
+        var methods = type.GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.Static);
+
+        foreach (var method in methods)
+        {
+            if (method.CustomAttributes.Any(x => x.AttributeType == typeof(AuthorAttribute)))
+            {
+                //false - взема всички приложени атрибути, които не са онаследени
+                var attributes = method.GetCustomAttributes(false);
+
+                foreach (AuthorAttribute attribute in attributes)
+                {
+                    Console.WriteLine($"{method.Name} is written by {attribute.Name}");
+                }
+            }
+        }
+    }
+}
