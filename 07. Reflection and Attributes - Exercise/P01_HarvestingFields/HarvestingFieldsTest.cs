1b4d84a2bc1a66d9200799a8462e44a743c7300a
irinastoicheva <irinastoicheva@mail.bg>
2019-03-31 22:31:07 +0300

    commit

---
+++ 07. Reflection and Attributes - Exercise/P01_HarvestingFields/HarvestingFieldsTest.cs
@@ -0,0 +1,57 @@
+﻿ namespace P01_HarvestingFields
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+
+    public class HarvestingFieldsTest
+    {
+        public static void Main()
+        {
+            Type classInfo = typeof(HarvestingFields);
+
+            FieldInfo[] fields = classInfo.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+
+            string command = Console.ReadLine();
+
+            while (command != "HARVEST")
+            {
+                FieldInfo[] sortedFields = SortedFields(command, fields);
+
+                foreach (FieldInfo field in sortedFields)
+                {
+                    var attribute = field.Attributes.ToString().ToLower();
+                    if (attribute == "family")
+                    {
+                        attribute = "protected";
+                    }
+                    else if (attribute == "assembly")
+                    {
+                        attribute = "internal";
+                    }
+
+                    Console.WriteLine($"{attribute} {field.FieldType.Name} {field.Name}");
+                }
+
+                command = Console.ReadLine();
+            }
+        }
+
+        public static FieldInfo[] SortedFields(string type, FieldInfo[] fields)
+        {
+            switch (type)
+            {
+                case "private":
+                   return fields = fields.Where(f => f.IsPrivate).ToArray();
+                case "protected":
+                   return fields = fields.Where(f => f.IsFamily).ToArray();
+                case "public":
+                   return fields = fields.Where(f => f.IsPublic).ToArray();
+                case "all":
+                    return fields;
+                default:
+                    return null;
+            }
+        }
+    }
+}
---
+++ 07. Reflection and Attributes - Exercise/P02_BlackBoxInteger/BlackBoxIntegerTests.cs
@@ -0,0 +1,37 @@
+﻿namespace P02_BlackBoxInteger
+{
+    using System;
+    using System.Reflection;
+    using System.Linq;
+
+    public class BlackBoxIntegerTests
+    {
+        public static void Main()
+        {
+            Type classInfo = typeof(BlackBoxInteger);
+
+            BlackBoxInteger instance = (BlackBoxInteger)Activator.CreateInstance(classInfo, true);
+
+            MethodInfo[] methods = classInfo.GetMethods(BindingFlags.NonPublic | BindingFlags.Instance);
+
+            string input = Console.ReadLine();
+
+            while (input != "END")
+            {
+                string[] commandLine = input.Split("_");
+                string command = commandLine[0];
+                int value = int.Parse(commandLine[1]);
+
+                MethodInfo method = methods.Where(x => x.Name == command).First();
+
+                method.Invoke(instance, new object [] { value});
+
+                var innerValue = classInfo.GetFields(BindingFlags.NonPublic | BindingFlags.Instance).Where(x => x.Name == "innerValue").First();
+
+                Console.WriteLine(innerValue.GetValue(instance));
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
