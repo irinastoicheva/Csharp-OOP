274cfc7cf793c718ba5e8c394aac48ebcce5e713
irinastoicheva <irinastoicheva@mail.bg>
2019-04-02 02:52:11 +0300

    commit

---
+++ 07. Reflection and Attributes - Exercise/P08.CreateCustomClassAttribute/Startup.cs
@@ -0,0 +1,13 @@
+﻿namespace P08.CreateCustomClassAttribute
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
---
+++ 07. Reflection and Attributes - Exercise/P08.CreateCustomClassAttribute/Weapon.cs
@@ -0,0 +1,8 @@
+﻿namespace P08.CreateCustomClassAttribute
+{
+
+    [InfoAttribute("Pesho", 3, "Used for C# OOP Advanced Course - Enumerations and Attributes.", "Pesho", "Svetlio")]
+    public abstract class Weapon
+    {
+    }
+}
