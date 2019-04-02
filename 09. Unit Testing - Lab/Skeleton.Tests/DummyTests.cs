ebc5215bc726bb5ea1d02f2d4f070035175a6b6f
irinastoicheva <irinastoicheva@mail.bg>
2019-04-02 23:52:01 +0300

    commit

--- 09. Unit Testing - Lab/Skeleton.Tests/DummyTests.cs
+++ 09. Unit Testing - Lab/Skeleton.Tests/DummyTests.cs
@@ -7,23 +7,19 @@
     [TestFixture]
     public class DummyTests
     {
-        [SetUp]
-        public void SetUp()
-        {
-            //Изпълнява се автоматично преди всеки тест
-        }
+        private Dummy dummy;
+        private int DummyHealth = 10;
+        private int DummyExperience = 10;
 
-        [TearDown]
-        public void TearDoun()
+        [SetUp]
+        public void TestInit()
         {
-            //Изпълнява се след всеки тест
+            this.dummy = new Dummy(DummyHealth, DummyExperience);
         }
 
         [Test]
         public void DummyLosesHealthIfAttacked()
         {
-            Dummy dummy = new Dummy(10, 10);
-
             dummy.TakeAttack(5);
 
             Assert.AreEqual(5, dummy.Health);
@@ -32,66 +28,27 @@
         [Test]
         public void DeadDummyThrowsExceptionIfAttacked()
         {
-            Dummy dummy = new Dummy(0, 5);
+            Dummy dummy = new Dummy(0, this.DummyExperience);
 
-            Assert.Throws<InvalidOperationException>(() => dummy.TakeAttack(2));
+            Assert.That(() => dummy.TakeAttack(5), Throws.InvalidOperationException.With.Message.EqualTo("Dummy is dead."));
         }
 
         [Test]
         public void DeadDummyCanGiveXP()
         {
-            Dummy dummy = new Dummy(1, 5);
+            Dummy dummy = new Dummy(1, this.DummyExperience);
 
-            Assert.Throws<InvalidOperationException>(() => dummy.GiveExperience());
+            Assert.Throws<InvalidOperationException>(() => dummy.GiveExperience(), "Dead dummy doesn't give experience.");
         }
 
         [Test]
         public void AliveDummyCanNotGiveXP()
         {
-            Dummy dummy = new Dummy(-1, 5);
+            Dummy dummy = new Dummy(-1, this.DummyExperience);
 
             int experience = dummy.GiveExperience();
 
-            Assert.AreEqual(5, experience);
+            Assert.AreEqual(10, experience);
         }
-
-
-
-        //[Test]
-        //public void DeadDummy()
-        //{
-        //    //Arrange
-        //    Dummy dummy = new Dummy(0, 10);
-
-        //    //Act + Assert
-        //    Assert.Throws<InvalidOperationException>(() => dummy.TakeAttack(2));
-        //}
-
-        //[Test]
-        //[TestCase(20, true)]
-        //[TestCase(10, false)]
-        //[TestCase(100, true)]
-        //public void GiveExperience(int dummyHealth, string property)
-        //{
-        //    //Arrange
-        //    Dummy dummy = new Dummy(dummyHealth, 10);
-
-        //    //Act 
-        //    int exp = dummy.GiveExperience();
-
-        //    //Assert
-        //    exp.Should().Be(10);
-        //}
-
-        //[Test]
-        //public void CantGiveExperionce()
-        //{
-        //    //Arrange
-        //    Dummy dummy = new Dummy(2, 10);
-
-        //    //Act 
-        //    //Assert
-        //    Assert.Throws<InvalidOperationException>(() => dummy.GiveExperience());
-        //}
     }
 }
